381c26c0993c8de424710d1ce88e956831bd453d
and7ey <and7ey@gmail.com>
2025-01-16 12:38:46 +0300

    временный файл для тестирования кодов клавиш

---
+++ keys_test.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    Lampa.Keypad.listener.follow('keydown', function (e) {
+        var code = e.code;
+        Lampa.Noty.show('code_ '+ code);
+    });
+    
+
+})();
