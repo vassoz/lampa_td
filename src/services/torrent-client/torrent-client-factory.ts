ffc6175738e3b289b78023f13f65a5ea6e319ac5
kvart714 <17424528+kvart714@users.noreply.github.com>
2025-05-19 02:24:18 +0400

    mult-server

--- src/services/torrent-client/torrent-client-factory.ts
+++ src/services/torrent-client/torrent-client-factory.ts
@@ -1,25 +1,58 @@
-import type { ITorrentClient } from '../../../types/torrent-client';
-import { CLIENT_TYPE_KEY, URL_KEY, LOGIN_KEY, PASSWORD_KEY } from '../../settings';
-import { QBittorrentWebApiClient } from './qbit/qbittorrent-webapi-client';
-import { TransmissionService } from './transmission/transmission';
+import type { ITorrentClient } from '../../../types/torrent-client'
+import {
+    CLIENT_TYPE_KEY,
+    URL_KEY,
+    LOGIN_KEY,
+    PASSWORD_KEY,
+} from '../../settings'
+import { QBittorrentWebApiClient } from './qbit/qbittorrent-webapi-client'
+import { TransmissionService } from './transmission/transmission'
 
 export class TorrentClientFactory {
-    private static client?: ITorrentClient;
+    private static client?: ITorrentClient
 
     public static getClient(): ITorrentClient {
         if (!this.client) {
-            const useQbittorrent = Lampa.Storage.field(CLIENT_TYPE_KEY) === 1;
-            const url = Lampa.Storage.field(URL_KEY);
-            const login = Lampa.Storage.field(LOGIN_KEY);
-            const password = Lampa.Storage.field(PASSWORD_KEY);
-            this.client = useQbittorrent
-                ? new QBittorrentWebApiClient(url, login, password)
-                : new TransmissionService(url, login, password);
+            const url = Lampa.Storage.field(URL_KEY)
+
+            const urls = url.split(';')
+            if (urls.length === 1) {
+                TorrentClientFactory.buildClient(url)
+            }
+
+            if (urls.length > 1) {
+                TorrentClientFactory.selectUrl(urls)
+            }
         }
-        return this.client;
+        return this.client!
     }
 
     public static reset(): void {
-        this.client = undefined;
+        this.client = undefined
+    }
+
+    private static buildClient(url: any) {
+        const useQbittorrent = Lampa.Storage.field(CLIENT_TYPE_KEY) === 1
+        const login = Lampa.Storage.field(LOGIN_KEY)
+        const password = Lampa.Storage.field(PASSWORD_KEY)
+        this.client = useQbittorrent
+            ? new QBittorrentWebApiClient(url, login, password)
+            : new TransmissionService(url, login, password)
+    }
+
+    private static async selectUrl(urls: string[]) {
+        for (const url of urls) {
+            try {
+                const result = await fetch(url + '/ping', {
+                    cache: 'no-cache',
+                })
+                if (result.ok) {
+                    this.buildClient(url)
+                    return
+                }
+            } catch {}
+        }
+
+        this.buildClient(urls[0])
     }
 }
